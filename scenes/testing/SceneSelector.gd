6df59c73af970d1b2a68bb7552166d9a0db8e862
Dominik David <domo23000@gmail.com>
2026-06-06 19:51:08 -0600

    scene selector

---
+++ scenes/testing/SceneSelector.gd
@@ -0,0 +1,25 @@
+extends Control
+
+@export var container: VBoxContainer
+
+var scene_list: Array = [
+	"res://scenes/testing/test_environment.tscn",
+	"res://scenes/ship_building/ship_builder_interface.tscn"
+]
+
+
+func _ready():
+	create_buttons()
+
+
+func create_buttons():
+	for scene in scene_list:
+		var button = Button.new()
+		button.text = scene.get_file().get_basename()
+		button.pressed.connect(load_scene.bind(scene))
+		container.add_child(button)
+	container.reset_size()
+
+
+func load_scene(scene: String):
+	get_tree().change_scene_to_file(scene)
